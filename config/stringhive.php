72a647ce35e4c3793a0e38eb7b5c3d9676983cfe
Daniel <daniel@blendbyte.com>
2026-04-28 18:37:08 +0800

    Update default base URL to stringhive.com

--- config/stringhive.php
+++ config/stringhive.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'base_url' => env('STRINGHIVE_URL', 'https://stringhive.test'),
+    'base_url' => env('STRINGHIVE_URL', 'https://www.stringhive.com'),
     'token'    => env('STRINGHIVE_TOKEN'),
     'timeout'  => 30,
 ];
